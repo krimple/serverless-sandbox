5b2d64d2d944cbe85d6af941991618a4a5844ffc
Ken Rimple <krimple@chariotsolutions.com>
2020-02-25 13:32:27 -0500

    added pathing

--- handler.ts
+++ handler.ts
@@ -2,11 +2,35 @@ import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
 export const shout: APIGatewayProxyHandler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'AAAAHHH!',
-      input: event,
-    }, null, 2),
-  };
-}
+    /*  console.log('Got event from my call. Body is:');
+      console.log(JSON.stringify(event.body));
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: `AAAAHHH ${event.body}!`,
+          input: event,
+        }, null, 2),
+      };
+
+     */
+
+    let message: string;
+
+    switch (event.pathParameters.key) {
+        case 'shk':
+            message = 'AAAAHH SHARK!';
+            break;
+        case 'py':
+            message = 'AAAAHH PYTHON!';
+            break;
+        default:
+            message = 'AAAAHH LAZY!';
+    }
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: `AAAAHHH ${message}`
+        })
+    };
+};
